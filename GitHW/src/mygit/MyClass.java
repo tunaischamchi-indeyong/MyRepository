0b82e9a66a5a14411964726b0ee12acaa22090f0
사용자 <@DESKTOP-CU6G5N4>
2025-12-04 14:22:12 +0900

    First commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,5 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("2nd version");
+		YourClass your=new YourClass();
+		your.sayHello("Everyone");
 	}
 }
