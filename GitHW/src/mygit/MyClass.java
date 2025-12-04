5fcc0791679d01944be2747648d40f06d39b2592
사용자 <@DESKTOP-CU6G5N4>
2025-12-04 14:27:04 +0900

    change in master branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("2nd version");
+		System.out.println("My changed in master branch");
 	}
 }
